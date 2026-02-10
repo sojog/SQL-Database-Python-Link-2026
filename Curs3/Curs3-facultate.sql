5ad6a41bf3d1281c4276d683b30da95e1d066b27
Silviu Ojog <sil.ojog@gmail.com>
2026-02-10 20:14:17 +0200

    Connect to MySql with secrets

---
+++ Curs3/Curs3-facultate.sql
@@ -0,0 +1,14 @@
+
+
+SELECT * FROM `Facultate`.`Student`;
+INSERT INTO `Facultate`.`Student` SET nume = 'Andreescu', prenume = 'Andrei';
+
+SELECT * FROM `Facultate`.`Student`;
+
+INSERT INTO `Facultate`.`Student` SET nume = 'Andreescu', prenume = 'Andreea';
+SELECT * FROM `Facultate`.`Student`;
+
+INSERT INTO `Facultate`.`Student` SET nume = 'Andreescu', prenume = 'Andrei';
+
+INSERT INTO `Facultate`.`Student` SET nume = 'Ionescu', prenume = 'Andrei';
+SELECT * FROM `Facultate`.`Student`;
